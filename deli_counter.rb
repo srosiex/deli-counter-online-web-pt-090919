4619c224b119825fe2277d0ec133e39baaf41a72
Sarah H <sarahrose.henderson@gmail.com>
2019-09-11 16:27:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(numinline)
   else
     line_method_array.each.with_index(0) do |name, index|
       line_method_array.push("#{index}. #{name}")
-      # push method will push the name into the index 
+      # push method will push the name into the index
     end
     puts "The line is: #{line_method_array.join(" ")}"
   end
