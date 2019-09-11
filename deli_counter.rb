793fba703eb6a4ab1b752dfe8254f69a6dd877bb
Sarah H <sarahrose.henderson@gmail.com>
2019-09-11 17:14:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,11 +12,7 @@ def line(x)
   end
 end
 
-def take_a_number(line, name)
-  new_array = []
-line.each.with_index(1) do |name, index|
-new_array << "#{name}, #{index}"
-end
-puts "Welcome, #{name}. You are number #{index} in line."
-end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
