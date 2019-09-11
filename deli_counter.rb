d0230178f2eb6e61057bb72a9b3d44b744899d11
Sarah H <sarahrose.henderson@gmail.com>
2019-09-11 15:54:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,3 @@
-katz_deli = []
-other_deli = ["Logan", "Avi", "Spencer"]
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(x)
   line_array = []
