4222a79798652cfd6ba9248f3cb4a27d155300ec
Sarah H <sarahrose.henderson@gmail.com>
2019-09-11 15:51:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
-def line
-
+def line(katz_deli)
+  puts "The line is currently empty."
 end
+line(katz_deli)
