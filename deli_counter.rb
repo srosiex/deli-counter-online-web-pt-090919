35e9f014e61b0b921be218fd0a87f5b31319a7c6
Sarah H <sarahrose.henderson@gmail.com>
2019-09-11 15:54:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(x)
-if x == 0
+  line_array = []
+if x.length == 0
   puts "The line is currently empty."
 end
 
